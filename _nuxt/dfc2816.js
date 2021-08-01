aced4685e9484ce6e78cf219706660c15b0bdf16
souvikjoy <souviksahajoy@gmail.com>
2021-08-02 05:24:48 +0600

    ssss

---
+++ _nuxt/dfc2816.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{351:function(e,t,n){"use strict";n.r(t);var c={name:"About"},r=n(31),component=Object(r.a)(c,(function(){var e=this,t=e.$createElement;return(e._self._c||t)("div",{staticClass:"grid place-items-center h-screen",attrs:{id:"about"}},[e._v("\n  About Me\n")])}),[],!1,null,null,null);t.default=component.exports}}]);
